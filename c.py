443fc1e6122bda78add2f25cdeb81c2042bf950b
richardwong_ <shigenkawasaki@gmail.com>
2024-06-04 23:17:38 +0900

    Add files via upload

--- c.py
+++ c.py
@@ -3,62 +3,88 @@ import sys
 import os
 import hashlib
 
-# protocol_header()という関数は、サーバに送信されるファイルのヘッダ情報をフォーマットするために使用されます。このヘッダは、ファイル名の長さ（バイト）、JSONデータの長さ（バイト）、データの長さ（バイト）の3つの値で構成されます。これらの値は、to_bytes()メソッドを用いてバイナリに変換され、1つの64ビットバイナリに結合されます。
-def protocol_header(filename_length, data_length, hash_length, json_length):
-    return filename_length.to_bytes(4, "big") + data_length.to_bytes(4,"big") + hash_length.to_bytes(32,"big") + json_length.to_bytes(4,"big")
+# protocol_header() function formats the header information of the file to be sent to the server.
+def protocol_header(filename, data_length, hash, json_length):
+    filename_length = 32  # Define the byte size for the filename
+    hash_length = 64  # Define the byte size for the hash (hexadecimal representation of SHA-256)
+    data_length_size = 4  # Fixed size for data length
+    json_size = 64  # Fixed size for JSON length
+
+    filename_bytes = filename.encode().ljust(filename_length, b'\x00')
+    return filename_bytes + data_length.to_bytes(data_length_size, "big") + hash.encode().ljust(hash_length, b'\x00') + json_length.to_bytes(json_size, "big")
 
 def hashFunc(filepath):
-    # Convert filepath to bytes before hashing
     with open(filepath, 'rb') as f:
-        file_data = f.read()  # Read the entire file as bytes
+        file_data = f.read()
     return hashlib.sha256(file_data).hexdigest()
 
 def upload():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # サーバが待ち受けているポートにソケットを接続します
+    # Connect the socket to the port where the server is listening
     server_address = '0.0.0.0'
     server_port = 9001
-    stream_rate = 4096*10
+    stream_rate = 4096 * 10
 
-    print('connecting to {}'.format(server_address, server_port))
+    print('connecting to {} port {}'.format(server_address, server_port))
 
     try:
-        # 接続後、サーバとクライアントが相互に読み書きができるようになります 
+        # After connection, the server and client can read and write to each other
         sock.connect((server_address, server_port))
     except socket.error as err:
         print(err)
         sys.exit(1)
 
     try:
-        # ファイルを送信する場合は、テキストファイルで2GB以下に制限してください
+        # File to be uploaded must be a text file and below 2GB
         filepath = input('Type in a file to upload: ')
 
-        # バイナリモードでファイルを読み込む
         with open(filepath, 'rb') as f:
-            # ファイルの末尾に移動し、tellは開いているファイルの現在位置を返します。ファイルのサイズを取得するために使用します
-            f.seek(0, os.SEEK_END)
-            filesize = f.tell()
-            f.seek(0,0)
-
-            if filesize > pow(2,32):
-                raise Exception('File must be below 2GB.')
-
             filename = os.path.basename(f.name)
 
-            # ファイル名からビット数
-            filename_bits = filename.encode('utf-8')
+            # Move to the end of the file to get its size
+            f.seek(0, os.SEEK_END)
+            filesize = f.tell()
+            f.seek(0, 0)
 
-            # protocol_header()関数を用いてヘッダ情報を作成し、ヘッダとファイル名をサーバに送信します。
-            # protocol_header(filename_length, data_length, hash_length, json_length)
-            header = protocol_header(len(filename_bits), filesize, hashFunc(filepath), 0)
-            return print(header)
+            if filesize > pow(2, 32):
+                raise Exception('File must be below 2GB.')
 
-            # ヘッダの送信、ファイル名の送信
+            hashedFile = hashFunc(filepath)
+
+            # Create header information using protocol_header() and send the header and filename to the server
+            header = protocol_header(filename, filesize, hashedFile, 0)
+            print(header)
+        
+
+            """
+            def extract_header(header):
+                filename_length = 32  # Define the byte size for the filename
+                hash_length = 64  # Define the byte size for the hash (hexadecimal representation of SHA-256)
+                data_length_size = 4  # Fixed size for data length
+                json_size = 64  # Fixed size for JSON length
+                
+                filename = header[:filename_length].rstrip(b'\x00').decode()  # Remove padding and decode
+                data_length = int.from_bytes(header[filename_length:filename_length + data_length_size], "big")
+                hash = header[filename_length + data_length_size:filename_length + data_length_size + hash_length].decode()
+                json = int.from_bytes(header[filename_length + data_length_size + hash_length:filename_length + data_length_size + hash_length + json_size], "big")
+                return filename, data_length, hash, json
+
+            # Extract the components from the header
+            extracted_filename, extracted_data_length, extracted_hash, extracted_json = extract_header(header)
+
+            print("Extracted Filename:", extracted_filename)
+            print("Extracted Data Length:", extracted_data_length)
+            print("Extracted Hash:", extracted_hash)
+            print("Extracted JSON:", extracted_json)
+
+            return print("well done")
+            """
+
+            # Send the header
             sock.send(header)
-            sock.send(filename_bits)
 
-            # 一度にstream_rateバイトずつ読み出し、送信することにより、ファイルを送信します。Readは読み込んだビットを返します
+            # Send the file data in chunks
             data = f.read(stream_rate)
             while data:
                 print("Sending...")
--- s.py
+++ s.py
@@ -9,50 +9,50 @@ def hashFunc(filepath):
         file_data = f.read()  # Read the entire file as bytes
     return hashlib.sha256(file_data).hexdigest()
 
+def extract_header(header):
+    filename_length = 32  # Define the byte size for the filename
+    hash_length = 64  # Define the byte size for the hash (hexadecimal representation of SHA-256)
+    data_length_size = 4  # Fixed size for data length
+    json_size = 64  # Fixed size for JSON length
+    
+    filename = header[:filename_length].rstrip(b'\x00').decode()  # Remove padding and decode
+    data_length = int.from_bytes(header[filename_length:filename_length + data_length_size], "big")
+    hash = header[filename_length + data_length_size:filename_length + data_length_size + hash_length].decode()
+    json = int.from_bytes(header[filename_length + data_length_size + hash_length:filename_length + data_length_size + hash_length + json_size], "big")
+    return filename, data_length, hash, json
+
+
 def upload():
     # まず、必要なモジュールをインポートし、ソケットオブジェクトを作成して、アドレスファミリ（AF_INET）とソケットタイプ（SOCK_STREAM）を指定します。サーバのアドレスは、任意のIPアドレスからの接続を受け入れるアドレスである0.0.0.0に設定し、サーバのポートは9001に設定されています。
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = '0.0.0.0'
     server_port = 9001
+    stream_rate = 4096*10
 
     # 次に、現在の作業ディレクトリに「temp」という名前のフォルダが存在するかどうかをチェックします。存在しない場合は、os.makedirs() 関数を使用してフォルダを作成します。このフォルダは、クライアントから受信したファイルを格納するために使用されます。
     dpath = 'temp'
     if not os.path.exists(dpath):
         os.makedirs(dpath)
-
     print('Starting up on {} port {}'.format(server_address, server_port))
 
     # 次に、サーバは bind()関数を使用して、ソケットをサーバのアドレスとポートに紐付けします。その後、listen()関数を呼び出すことで、サーバは着信接続の待ち受けを開始します。サーバは一度に最大1つの接続を受け入れることができます。
     sock.bind((server_address, server_port))
-
     sock.listen(1)
 
     while True:
         # その後、サーバは無限ループに入り、クライアントからの着信接続を継続的に受け付けます。このコードでは、accept()関数を使用して、着信接続を受け入れ、クライアントのアドレスを取得します。
+
         connection, client_address = sock.accept()
         try:
             print('connection from', client_address)
             # 次に、クライアントから受信したデータのヘッダを読み取り、変数headerに格納します。ヘッダには、ファイル名の長さ、JSON データの長さ、クライアントから受信するデータの長さに関する情報が含まれています。
             header = connection.recv(8)
+            filename, data_length, hashedFile, json = extract_header(header)
 
-            # 長さはヘッダから抽出され、別々の変数に格納されます。
-            filename_length = int.from_bytes(header[:1], "big")
-            json_length = int.from_bytes(header[1:3], "big")
-            data_length = int.from_bytes(header[4:8], "big")
-            stream_rate = 4096*10
-
-            print('Received header from client. Byte lengths: Title length {}, JSON length {}, Data Length {}'.format(filename_length, json_length,data_length))
-
-            # 次に、クライアントからファイル名を読み取り、変数に格納します。JSONデータがある場合は、サポートされていないため、例外が発生します。受信するデータがない場合、コードは例外を発生させます。
-            filename = connection.recv(filename_length).decode('utf-8')
-
-            print('Filename: {}'.format(filename))
-
-            if json_length != 0:
-                raise Exception('JSON data is not currently supported.')
-
-            if data_length == 0:
-                raise Exception('No data to read from client.')
+            print("Extracted Filename:", filename)
+            print("Extracted Data Length:", data_length)
+            print("Extracted Hash:", hash)
+            print("Extracted JSON:", json)
 
             # 次に、コードはクライアントから受け取ったファイル名で新しいファイルをtempフォルダに作成します。このファイルは、withステートメントを使用してバイナリモードで開かれ、write()関数を使用して、クライアントから受信したデータをファイルに書き込みます。データはrecv()関数を使用して塊単位で読み込まれ、データの塊を受信するたびにデータ長がデクリメントされます。
             # w+は終了しない場合はファイルを作成し、そうでない場合はデータを切り捨てます
