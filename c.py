a962a8f00093f6609a2c8cb5934ae75bb8def4b0
richardwong_ <shigenkawasaki@gmail.com>
2024-06-04 21:05:50 +0900

    Add files via upload

--- c.py
+++ c.py
@@ -1,19 +1,26 @@
 import socket
 import sys
 import os
+import hashlib
 
 # protocol_header()という関数は、サーバに送信されるファイルのヘッダ情報をフォーマットするために使用されます。このヘッダは、ファイル名の長さ（バイト）、JSONデータの長さ（バイト）、データの長さ（バイト）の3つの値で構成されます。これらの値は、to_bytes()メソッドを用いてバイナリに変換され、1つの64ビットバイナリに結合されます。
-def protocol_header(filename_length, data_length):
-    return filename_length.to_bytes(1, "big") + data_length.to_bytes(3,"big")
+def protocol_header(filename_length, data_length, hash_length, json_length):
+    return filename_length.to_bytes(4, "big") + data_length.to_bytes(4,"big") + hash_length.to_bytes(32,"big") + json_length.to_bytes(4,"big")
 
+def hashFunc(filepath):
+    # Convert filepath to bytes before hashing
+    with open(filepath, 'rb') as f:
+        file_data = f.read()  # Read the entire file as bytes
+    return hashlib.sha256(file_data).hexdigest()
 
-def main():
+def upload():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # サーバが待ち受けているポートにソケットを接続します
-    packetSize = 4096
     server_address = '0.0.0.0'
     server_port = 9001
+    stream_rate = 4096*10
+
     print('connecting to {}'.format(server_address, server_port))
 
     try:
@@ -24,10 +31,8 @@ def main():
         sys.exit(1)
 
     try:
-        # ファイルを送信する場合は、2^32までに制限してください
+        # ファイルを送信する場合は、テキストファイルで2GB以下に制限してください
         filepath = input('Type in a file to upload: ')
-        if print(filepath[-4:]) != '.mp4':
-            return print("Unsupported file type. Please upload .mp4")
 
         # バイナリモードでファイルを読み込む
         with open(filepath, 'rb') as f:
@@ -36,7 +41,6 @@ def main():
             filesize = f.tell()
             f.seek(0,0)
 
-            # ファイルサイズが大きくなりすぎることの警告
             if filesize > pow(2,32):
                 raise Exception('File must be below 2GB.')
 
@@ -44,23 +48,25 @@ def main():
 
             # ファイル名からビット数
             filename_bits = filename.encode('utf-8')
-            # protocol_header()関数を用いてヘッダ情報を作成し、ヘッダとファイル名をサーバに送信します。
-            header = protocol_header(len(filename_bits), filesize)
 
-            # ヘッダの送信
+            # protocol_header()関数を用いてヘッダ情報を作成し、ヘッダとファイル名をサーバに送信します。
+            # protocol_header(filename_length, data_length, hash_length, json_length)
+            header = protocol_header(len(filename_bits), filesize, hashFunc(filepath), 0)
+            return print(header)
+
+            # ヘッダの送信、ファイル名の送信
             sock.send(header)
-            # ファイル名の送信
             sock.send(filename_bits)
 
-            # 一度にpacketSizeずつ読み出し、送信することにより、ファイルを送信します。Readは読み込んだビットを返します
-            data = f.read(packetSize)
+            # 一度にstream_rateバイトずつ読み出し、送信することにより、ファイルを送信します。Readは読み込んだビットを返します
+            data = f.read(stream_rate)
             while data:
                 print("Sending...")
                 sock.send(data)
-                data = f.read(packetSize)
+                data = f.read(stream_rate)
 
     finally:
         print('closing socket')
         sock.close()
 
-main()
+upload()
