b3f07735ed8d2df2b62f3c321109f5f8d3019d64
richardwong_ <shigenkawasaki@gmail.com>
2024-06-08 23:03:39 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import video_editor
-
-# Example usage:
-# video_editor.convert_to_mp3('cat.mp4')
-# video_editor.compressSize('cat.mp4')
-# video_editor.changeResolution('cat.mp4', 1280, 720)
-# video_editor.changeAspectRatio('cat.mp4', '16:9')
-# video_editor.convert_to_GIF('cat.mp4', '00:00:10', '5')
-video_editor.change_speed('cat.mp4', 2.0)
